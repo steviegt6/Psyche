3d5255fda790a219d0a9166a8fb629fbaeddc9dc
Steviegt6 <xxlennygamerxx@gmail.com>
2021-12-16 15:30:46 -0800

    More precise FPS display.

--- source/Main.hx
+++ source/Main.hx
@@ -1,11 +1,13 @@
 package;
 
+import openfl.display.FPS;
+import flixel.group.FlxGroup;
+import psyche.PsycheFpsDisplay;
 import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxState;
 import openfl.Assets;
 import openfl.Lib;
-import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 
@@ -18,7 +20,7 @@ class Main extends Sprite
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-	public static var fpsVar:FPS;
+	public static var fpsVar:PsycheFpsDisplay;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
@@ -73,8 +75,10 @@ class Main extends Sprite
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
 		#if !mobile
-		fpsVar = new FPS(10, 3, 0xFFFFFF);
+		fpsVar = new PsycheFpsDisplay(10, 3);
+
 		addChild(fpsVar);
+
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
